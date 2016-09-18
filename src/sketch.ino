bf97d800aa27b3e58442d60dc1c97b7501f68d4a
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2016-09-18 21:32:18 +0900

    Implement SG90 sample using Servo class.

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,8 +1,30 @@
+#include <Servo.h>
+
+#define ANGLE_OFFSET 1
+
+Servo servo;
 
 void setup()
 {
+    Serial.begin(9600);
+    servo.attach(4);
 }
 
 void loop()
 {
+    static int angle;
+    static int offset = ANGLE_OFFSET;
+    angle += offset;
+    if (angle > 180) {
+        angle  = 180;
+        offset = -offset;
+    }
+    if (angle < 0) {
+        angle  = 0;
+        offset = -offset;
+    }
+
+    Serial.print("servo : ");
+    Serial.println(angle);
+    servo.write(angle);
 }
