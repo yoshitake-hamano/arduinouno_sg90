e80ae5f6fc5e9c043135956421ef512e28bd68ad
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2016-10-31 22:48:40 +0900

    Implement IR

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,64 +1,97 @@
 #include <Servo.h>
 #include <IRremote.h>
 
+const int IRRECV_PIN   = 11;
+const int STEERING_PIN = 4;
+
+const unsigned long IRRECV_LEFT    = 0x41B619E6;
+const unsigned long IRRECV_RIGHT   = 0x41B66996;
+const unsigned long IRRECV_UP      = 0x41B6F906;
+const unsigned long IRRECV_DOWN    = 0x41B67986;
+const unsigned long IRRECV_ENTER   = 0x41B659A6;
+const unsigned long IRRECV_SLEEP30 = 0x41B6A15E;
+const unsigned long IRRECV_SLEEP60 = 0x41B6619E;
+
 class SteeringController
 {
 private:
+    const int STEERING_MIN    = 66;
+    const int STEERING_INIT   = 78;
+    const int STEERING_MAX    = 90;
+    const int STEERING_OFFSET = 4;
+
     Servo servo;
+    int   index;
 
 public:
     void attach(int pin) {
+        index = 0;
         servo.attach(pin);
+        servo.write(STEERING_INIT);
     }
 
-    void set(int angle) {
-        servo.write(angle);
+    void right() {
+        int nextIndex = index + 1;
+        int nextAngle = nextIndex * STEERING_OFFSET + STEERING_INIT;
+        if (nextAngle > STEERING_MAX) {
+            return;
+        }
+        index = nextIndex;
+        servo.write(nextAngle);
     }
-};
 
-class Position
-{
-public:
-    int angle;
-
-    Position(int angle) {
-        this->angle = angle;
+    void left() {
+        int nextIndex = index - 1;
+        int nextAngle = nextIndex * STEERING_OFFSET + STEERING_INIT;
+        if (nextAngle < STEERING_MIN) {
+            return;
+        }
+        index = nextIndex;
+        servo.write(nextAngle);
     }
 };
 
 SteeringController steering;
+IRrecv irrecv(IRRECV_PIN);
+
+void dump(decode_results* results)
+{
+    int count = results->rawlen;
+    if (results->decode_type == UNKNOWN) {
+        Serial.print("Unknown encodint: ");
+    } else if (results->decode_type == NEC) {
+        Serial.print("Decorded NEC: ");
+    } else {
+        Serial.print("Unknown encodint: ");
+    }
+    Serial.print(results->value, HEX);
+    Serial.print(" (");
+    Serial.print(results->bits, DEC);
+    Serial.print("bits)");
+    Serial.print("Raw (");
+    Serial.print(count, DEC);
+    Serial.println(")");
+}
 
 void setup()
 {
     Serial.begin(9600);
-    steering.attach(4);
+    steering.attach(STEERING_PIN);
+    irrecv.enableIRIn();
 }
 
 void loop()
 {
-#if 1
-    int x = 0;
-    while (x == 0) {
-        x = Serial.parseInt();
+    decode_results results;
+    if (!irrecv.decode(&results)) {
+        return;
     }
-    Serial.println(x);
-    steering.set(x);
-    delay(250);
-#else
-    Position pos[] = {
-        {70},
-        {75},
-        {80}, // center
-        {85},
-        {90},
-        {85},
-        {80}, // center
-        {75},
-        };
-    int size = sizeof(pos) / sizeof(pos[0]);
-    for (int i=0; i<size; i++) {
-        steering.set(pos[i].angle);
-        delay(1000);
+
+    dump(&results);
+    irrecv.resume();
+    switch (results.value) {
+    case IRRECV_LEFT:  steering.left();  break;
+    case IRRECV_RIGHT: steering.right(); break;
     }
-#endif
+    delay(50);
 }
